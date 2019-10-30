ff143c08c2607e6962e007c3b560e9071871a021
kernelthree <j.v.julien@gmail.com>
2019-10-29 23:46:55 -0400

    Implement s1

--- solutions/s1/src/main.rs
+++ solutions/s1/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let mut sum : u32 = 0;
+
+    for x in 3..1000 {
+        if x % 3 == 0 || x % 5 == 0 {
+            sum += x;
+        }
+    }
+
+    println!("{}", sum);
 }
